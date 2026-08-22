3e75796e3f56db50f8b9116f41ccfcb64df13246
Shakhaoath Pappu <shakhaoathpappu@gmail.com>
2026-08-22 22:14:23 +0600

    Print

---
+++ 2. Print/1_char_print.asm
@@ -0,0 +1,20 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+
+.CODE
+MAIN PROC
+
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV AH, 02H
+    MOV DL, 'A'
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
