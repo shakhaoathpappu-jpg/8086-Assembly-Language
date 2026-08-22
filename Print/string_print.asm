c9c481699f5c47f3e060ef47ea991089ff6276cc
Shakhaoath Pappu <shakhaoathpappu@gmail.com>
2026-08-22 22:05:50 +0600

    Print String

---
+++ Print/string_print.asm
@@ -0,0 +1,21 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+    MSG DB 'Hello World!$'
+
+.CODE
+MAIN PROC
+
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV AH, 09H
+    LEA DX, MSG
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
